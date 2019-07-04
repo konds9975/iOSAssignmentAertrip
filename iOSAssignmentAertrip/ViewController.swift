6a454878cce6791a218ef7816496e981e6cf8010
kondya <konds9975@gmail.com>
2019-07-04 22:27:42 +0530

    Initial Commit

---
+++ iOSAssignmentAertrip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSAssignmentAertrip
+//
+//  Created by Kondya on 04/07/19.
+//  Copyright © 2019 Fortune4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
