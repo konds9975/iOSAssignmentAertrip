211f8fd888f8cea8f98330334c255f4c1fcbc830
kondya <konds9975@gmail.com>
2019-07-04 23:01:34 +0530

    init

--- iOSAssignmentAertrip/ViewController.swift
+++ iOSAssignmentAertrip/ViewController.swift
@@ -10,10 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.segmentControl.removeAllSegments()
+       
     }
+    
+    @IBAction func firstBtnAction(_ sender: UIButton) {
+        
+        self.segmentControl.removeAllSegments()
+        for (index,value) in [0,1,2,3,4,5,6,7,8].enumerated() {
+           
+            
+            if index >= 4 {
+                self.segmentControl.setTitle("\(index-3)+ Stop", forSegmentAt:3)
+            }else{
+                 self.segmentControl.insertSegment(withTitle: "\(value) Stop", at: index, animated: true)
+            }
+        }
+        self.segmentControl.selectedSegmentIndex = 0
+        
+    }
+    
+    @IBAction func secondBtnAction(_ sender: UIButton) {
+        
+        self.segmentControl.removeAllSegments()
+        for (index,value) in [0,1,2].enumerated() {
+            self.segmentControl.insertSegment(withTitle: "\(value) Stop", at: index, animated: true)
+        }
+        self.segmentControl.selectedSegmentIndex = 0
+        
+    }
+    @IBAction func thirdBtnAction(_ sender: UIButton) {
+        
+        self.segmentControl.removeAllSegments()
+        for (index,value) in [0,1].enumerated() {
+            self.segmentControl.insertSegment(withTitle: "\(value) Stop", at: index, animated: true)
+        }
+        self.segmentControl.selectedSegmentIndex = 0
+       
+    }
+    
+    @IBAction func fourthBtnAction(_ sender: UIButton) {
+        
+        self.segmentControl.removeAllSegments()
+        for (index,value) in [0].enumerated() {
+            self.segmentControl.insertSegment(withTitle: "\(value) Stop", at: index, animated: true)
+        }
+        self.segmentControl.selectedSegmentIndex = 0
+        
+    }
+    
 
 
 }
